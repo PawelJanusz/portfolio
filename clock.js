3f3d9171976ac4ead055bc432276973f5d32f4da
Pawel Janusz <marek80k@wp.pl>
2021-08-09 21:31:28 +0200

    navbar clock

---
+++ clock.js
@@ -0,0 +1,20 @@
+function counting() {
+
+    let today = new Date();
+
+    let hour = today.getHours()
+    if (hour < 10 )
+        hour = "0" + hour;
+
+    let minute = today.getMinutes()
+    if (minute < 10 )
+        minute = "0" + minute;
+
+    let second = today.getSeconds()
+    if (second < 10 )
+        second = "0" + second;
+
+    document.getElementById("clock").innerHTML = hour+":"+minute+":"+second;
+
+    setTimeout("counting()",1000)
+}
